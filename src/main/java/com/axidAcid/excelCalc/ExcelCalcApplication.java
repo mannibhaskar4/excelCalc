d7253beba84cb85534441c4a3edbb5172bf133bf
mannibhaskar4 <mannibhaskar4@gmail.com>
2023-09-25 08:12:22 +0530

    #1 Simple Hello world using spring-starter-web

---
+++ src/main/java/com/axidAcid/excelCalc/ExcelCalcApplication.java
@@ -0,0 +1,13 @@
+package com.axidAcid.excelCalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelCalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelCalcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/axidAcid/excelCalc/ExcelCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axidAcid.excelCalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelCalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
